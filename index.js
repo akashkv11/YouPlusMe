fdc304dca77f455d849887063ae623bf2635ef02
akashkv22 <akash@reontel.com>
2023-12-25 14:38:51 +0530

    gitignore changed

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js" 
+import { getDatabase,} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+  databaseURL:"https://youplusme-185d0-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 const heading = document.getElementById("heading");
